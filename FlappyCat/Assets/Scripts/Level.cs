632bf4518314e316fba1c76bc82d8e963bcad8fc
RobertW2 <bobbyweeden2@gmail.com>
2021-04-21 12:58:07 +1000

    3

--- FlappyCat/Assets/Scripts/Level.cs
+++ FlappyCat/Assets/Scripts/Level.cs
@@ -7,26 +7,71 @@ public class Level : MonoBehaviour
     private const float CAMERA_ORTHO_SIZE = 50f;
     private const float PIPE_WIDTH = 7.8f;
     private const float PIPE_HEAD_HEIGHT = 3.75f;
-    private const float PIPE_MOVE_SPEED = 10f;
+    private const float PIPE_MOVE_SPEED = 30f;
     private const float PIPE_DESTROY_X_POSITION = -100f;
+    private const float PIPE_SPAWN_X_POSITION = 100f;
+
+    private static Level instance;
+
+    public static Level GetInstance()
+    {
+        return instance;
+    }
+    
 
     private List<Pipe> pipeList;
+    private int pipesSpawned;
+    private float pipeSpawnTimer;
+    private float pipeSpawnTimerMax;
+    private float gapSize;
+
+    public enum Difficulty
+    {
+        Easy,
+        Medium,
+        Hard,
+        Impossible,
+    }
 
     private void Awake()
     {
+        instance = this;
         pipeList = new List<Pipe>();
+        pipeSpawnTimerMax = 1f;
+        SetDifficulty(Difficulty.Easy);
     }
 
     private void Start()
     {
         //CreatePipe(40f, 20f, true);
       //  CreatePipe(40f, 20f, false);
-        CreateGapPipes(50f, 20f, 20f);
+     //   CreateGapPipes(50f, 20f, 20f);
     }
 
     private void Update()
     {
-        HandlePipeMovement(); 
+        HandlePipeMovement();
+        HandlePipeSpawning();
+    }
+
+    private void HandlePipeSpawning()
+    {
+        pipeSpawnTimer -= Time.deltaTime;
+        if(pipeSpawnTimer < 0)
+        {
+            // time to spawn another pipe
+            pipeSpawnTimer += pipeSpawnTimerMax;
+
+            float heightEdgeLimit = 10f;
+            float minHeight = gapSize * .5f + heightEdgeLimit;
+            float totalHeight = CAMERA_ORTHO_SIZE * 2f;
+            float maxHeight = totalHeight - gapSize * .5f - heightEdgeLimit;
+
+            float height = Random.Range(minHeight, maxHeight);
+            CreateGapPipes(height, gapSize, PIPE_SPAWN_X_POSITION);
+        
+
+        }
     }
 
     private void HandlePipeMovement()
@@ -49,10 +94,42 @@ public class Level : MonoBehaviour
         
     }
 
+    private void SetDifficulty(Difficulty difficulty)
+    {
+        switch(difficulty)
+        {
+            case Difficulty.Easy:
+                gapSize = 50f;
+                pipeSpawnTimerMax = 1.2f;
+                break;
+            case Difficulty.Medium:
+                gapSize = 40f;
+                pipeSpawnTimerMax = 1.1f;
+                break;
+            case Difficulty.Hard:
+                gapSize = 33f;
+                pipeSpawnTimerMax = 1.0f;
+                break;
+            case Difficulty.Impossible:
+                gapSize = 24f;
+                pipeSpawnTimerMax = 0.8f;
+                break;
+        }
+    }
+    private Difficulty GetDifficulty()
+    {
+        if (pipesSpawned >= 30) return Difficulty.Impossible;
+        if (pipesSpawned >= 20) return Difficulty.Hard;
+        if (pipesSpawned >= 10) return Difficulty.Medium;
+        return Difficulty.Easy;
+    }
+
     private void CreateGapPipes(float gapY, float gapSize, float xPosition)
     {
         CreatePipe(gapY - gapSize * .5f, xPosition, true);
         CreatePipe(CAMERA_ORTHO_SIZE * 2f - gapY - gapSize * .5f, xPosition, false);
+        pipesSpawned++;
+        SetDifficulty(GetDifficulty());
     }
 
     private void CreatePipe(float height, float xPosition, bool createBottom)
@@ -106,6 +183,10 @@ public class Level : MonoBehaviour
         pipeList.Add(pipe);
     }
 
+    public int GetPipesSpawned()
+    {
+        return pipesSpawned;
+    }
    
     // Represents Single Pipe
     private class Pipe
---
+++ FlappyCat/Assets/Scripts/ScoreWindow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ScoreWindow : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI scoreText;
+
+    private void Awake()
+    {
+
+    }
+
+    private void Update()
+    {
+        scoreText.text = Level.GetInstance().GetPipesSpawned().ToString();
+    }
+}
