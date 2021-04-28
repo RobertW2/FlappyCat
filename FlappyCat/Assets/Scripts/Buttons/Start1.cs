c52c442a4c63d5a6aae8182724d9b0cb7ab9f8f8
RobertW2 <bobbyweeden2@gmail.com>
2021-04-28 11:11:23 +1000

    almost done

---
+++ FlappyCat/Assets/Scripts/Buttons/Start1.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Start1 : MonoBehaviour
+{
+	
+
+
+
+	public void TaskOnClick()
+	{
+		SceneManager.LoadScene("SampleScene");
+
+	}
+
+
+
+}
